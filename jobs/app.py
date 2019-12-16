c2451bb3d121493486a1e693acba39607cd69836
Andreya Valabek <avalabek@yahoo.com>
2019-12-16 11:55:15 -0700

    fix typos

--- jobs/app.py
+++ jobs/app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
-    if connection === None:
+    if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-    connection.row_factory  sqlite3.Row
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
@@ -18,12 +18,14 @@ def execute_sql(sql, values=(), commit=False, single=False):
     if commit == True:
         results = connection.commit()
     else:
-        results = cursor.fetchone() if single else curor.fetchall()
+        results = cursor.fetchone() if single else cursor.fetchall()
+        
         cursor.close()
         return results
 
 @app.teardown_appcontext
 def close_connection(exception):
+    connection = getattr(g, 'connection', None)
     connection = is not None:
     connection.close()
 
